4060238f780db5a3f83be088c2673d31b40d0e8f
Kjartan Maraas <kmaraas@gnome.org>
2006-12-06 01:18:38 +0000

    Port to automake 1.9. Patch from Christian Persch. Closes bug #131478
    2006-12-06  Kjartan Maraas  <kmaraas@gnome.org>
    
    	* .cvsignore:
    	* Makefile.am:
    	* autogen.sh:
    	* configure.in:
    	Port to automake 1.9. Patch from Christian Persch.
    	Closes bug #131478

--- autogen.sh
+++ autogen.sh
@@ -1,63 +1,20 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
-# This was lifted from the Gimp, and adapted slightly by
-# Raph Levien <raph@acm.org>.
 
-DIE=0
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
-PROJECT=LibArt_LGPL
+PKG_NAME="libart-lgpl"
 
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have autoconf installed to compile $PROJECT."
-	echo "Download the appropriate package for your distribution,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-	DIE=1
+(test -f $srcdir/libart.h) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level $PKG_NAME directory"
+    exit 1
 }
 
-# Do we really need libtool?
-(libtool --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have libtool installed to compile $PROJECT."
-	echo "Get ftp://ftp.gnu.org/pub/gnu/libtool-1.2.tar.gz"
-	echo "(or a newer version if it is available)"
-	DIE=1
+which gnome-autogen.sh || {
+    echo "You need to install gnome-common from the GNOME CVS"
+    exit 1
 }
 
-(automake-1.4 --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have automake installed to compile $PROJECT."
-	echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.3.tar.gz"
-	echo "(or a newer version if it is available)"
-	DIE=1
-}
-
-if test "$DIE" -eq 1; then
-	exit 1
-fi
-
-if test -z "$*"; then
-	echo "I am going to run ./configure with no arguments - if you wish "
-        echo "to pass any to it, please specify them on the $0 command line."
-fi
-
-case $CC in
-xlc )
-    am_opt=--include-deps;;
-esac
-
-for dir in $srcdir
-do 
-  echo processing $dir
-  (cd $dir; \
-  aclocalinclude="$ACLOCAL_FLAGS"; \
-  aclocal-1.4 $aclocalinclude; \
-  autoheader; automake-1.4 --add-missing --gnu $am_opt; autoconf)
-done
-
-$srcdir/configure "$@"
-
-echo 
-echo "Now type 'make' to compile $PROJECT."
+REQUIRED_AUTOMAKE_VERSION=1.9 USE_GNOME2_MACROS=1 . gnome-autogen.sh
